5980c90ded23e1e1bd100d8f1b4a8498312aead4
Zupa <jonas_windmolders@hotmail.com>
2015-02-20 14:13:14 +0100

    AI naming and ( ) fix

--- app/server/getNoLogDeathLogs.php
+++ app/server/getNoLogDeathLogs.php
@@ -41,23 +41,29 @@ foreach($deathlogs2  as  $log){
 
     $result['time'] = $exploded[0] . " " . $exploded[1];
 
-    $exploded2 = explode("(",$exploded[2],2);
+    $exploded2 = explode(" (",$exploded[2],2);
 
     $result['killername'] = trim($exploded2[0]);
 
-    $exploded3 = explode(")",$exploded2[1],2);
+    $exploded3 = explode(") ",$exploded2[1],2);
 
     $result['killerpuid'] = trim($exploded3[0]);
 
+    if($result['killerpuid'] == null || $result['killerpuid'] == "" )
+        $result['killerpuid'] = "AI";
+
     $exploded3[1] = str_replace(" killed ","",$exploded3[1]);
 
-    $exploded4 = explode("(",$exploded3[1],2);
+    $exploded4 = explode(" (",$exploded3[1],2);
 
     $result['killedname'] = trim($exploded4[0]);
 
-    $exploded5 = explode(")",$exploded4[1],2);
+    $exploded5 = explode(") ",$exploded4[1],2);
 
     $result['killedpuid'] = trim($exploded5[0]);
+
+    if($result['killedpuid'] == null || $result['killedpuid'] == "" )
+        $result['killedname'] = "AI";
 
     $exploded5[1] = str_replace(" with weapon","",$exploded5[1]);
 
